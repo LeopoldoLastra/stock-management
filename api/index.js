4ee5b22b7b41f2c31a6eb0c53c631ffd3b743dbd
Leopoldo <leopoldolastra7@gmail.com>
2024-06-24 19:20:37 -0300

    Se modifica la estructura del proyecto para poder hacer el deploy del backend en Vercel

--- api/index.js
+++ api/index.js
@@ -2,13 +2,17 @@ const express = require('express');
 const app = express();
 const routerApi = require('./routes/router');
 const {logError, errorHandler, boomErrorHandler}= require('./middleware/error.handler')
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
 app.listen(port,()=>{
     console.log("Puerto " + port)
 });
+
+app.get('/api', (req,res)=>{
+    res.send('Mi server en express')
+})
 
 routerApi(app);
 
