948b99024b27a966f7cc49041b4fa34ffa4635e1
Leopoldo <leopoldolastra7@gmail.com>
2024-06-24 19:31:36 -0300

    Corrijiendo modificación en la estructura del proyecto

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const routerApi = require('./routes/router');
 const {logError, errorHandler, boomErrorHandler}= require('./middleware/error.handler')
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000 ;
 
 app.use(express.json());
 
